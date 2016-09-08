f369e327a44b25dd2ac46af46085717d6ee1e8d5
Henning Jacobs <henning@jacobs1.de>
2016-09-08 23:01:43 +0200

    template rendering

--- api/app.py
+++ api/app.py
@@ -1,46 +1,99 @@
 #!/usr/bin/env python3
 import connexion
 import datetime
+import json
 import logging
+import os
+import random
+import smtplib
+import string
 
 from connexion import NoContent
+from email.mime.text import MIMEText
 
-# our memory-only job storage
-jobS = {}
+DATA_PATH = os.getenv('DATA_PATH', 'data')
+
+logger = logging.getLogger('api')
+
+
+def random_id(prefix: str, length: int):
+    return prefix + ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(length))
+
+
+def send_confirmation_mail(job: dict, job_id: str, token: str):
+    text = '''
+Follow this link to confirm your job posting:
+https://freesoftwarejobs.org/confirm/{}/{}
+'''.format(job_id, token)
+
+    msg = MIMEText(text)
+    msg['Subject'] = 'Please confirm your job posting'
+    msg['From'] = 'no-reply@freesoftwarejobs.org'
+    msg['To'] = job['created_by']
+
+    logger.info('Sending confirmation mail for %s to %s..', job_id, msg['To'])
+    server = smtplib.SMTP('localhost')
+    server.send_message(msg)
+    server.quit()
+
 
 
 def get_jobs(limit):
-    pass
+    return NoContent, 501
 
 
 def get_job(job_id):
-    job = jobS.get(job_id)
-    return job or ('Not found', 404)
+    return NoContent, 501
 
 
-def post_job():
-    pass
+def post_job(job: dict):
+    # no way to properly validate email addresses, so let's keep it simple..
+    if '@' not in job.get('created_by', ''):
+        return 'Invalid email', 400
+
+    job_id = random_id('job-', 16)
+    token = random_id('', 20)
+
+    job['id'] = job_id
+    job['created'] = datetime.datetime.utcnow().isoformat()
+
+    with open(os.path.join(DATA_PATH, 'tokens', token + '.json'), 'w') as fd:
+        json.dump({'job_id': job_id}, fd)
+
+    with open(os.path.join(DATA_PATH, 'jobs', '.' + job_id + '.pending.json'), 'w') as fd:
+        json.dump(job, fd)
+
+    send_confirmation_mail(job, job_id, token)
+
+    return job_id, 201
+
+
+def confirm_job(job_id, token):
+    token_path = os.path.join(DATA_PATH, 'tokens', token + '.json')
+    try:
+        with open(token_path) as fd:
+            token_data = json.load(fd)
+        if token_data['job_id'] != job_id:
+            raise Exception('Invalid token')
+    except:
+        return 'Invalid token', 400
+
+    try:
+        os.rename(os.path.join(DATA_PATH, 'jobs', '.' + job_id + '.pending.json'), os.path.join(DATA_PATH, 'jobs', job_id + '.json'))
+    except FileNotFoundError:
+        return 'Invalid token', 400
+
+    os.unlink(token_path)
+
+    return 'Job {} was confirmed.'.format(job_id)
 
 
 def put_job(job_id, job):
-    exists = job_id in jobS
-    job['id'] = job_id
-    if exists:
-        logging.info('Updating job %s..', job_id)
-    else:
-        logging.info('Creating job %s..', job_id)
-        job['created'] = datetime.datetime.utcnow()
-    jobS[job_id] = job
-    return NoContent, (200 if exists else 201)
+    return NoContent, 501
 
 
 def delete_job(job_id):
-    if job_id in jobS:
-        logging.info('Deleting job %s..', job_id)
-        del jobS[job_id]
-        return NoContent, 204
-    else:
-        return NoContent, 404
+    return NoContent, 501
 
 
 logging.basicConfig(level=logging.INFO)
---
+++ render.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import hashlib
+import jinja2
+import json
+import os
+import re
+import struct
+import subprocess
+
+os.makedirs('output/jobs', exist_ok=True)
+subprocess.check_call('cp -r files/* output/', shell=True)
+
+data_path = os.getenv('DATA_PATH', 'api/data')
+jobs_path = os.path.join(data_path, 'jobs')
+
+jobs = {}
+
+for entry in os.listdir(jobs_path):
+    if entry.startswith('job-') and entry.endswith('.json'):
+        with open(os.path.join(jobs_path, entry)) as fd:
+            data = json.load(fd)
+        h = hashlib.new('sha1')
+        h.update(data['id'].encode('ascii'))
+        prefix = '{}-'.format(struct.unpack('i', h.digest()[:4])[0] % 10000)
+        data['slug'] = prefix + re.sub('[^a-z0-9-]', '-', data['title'].lower() + ' ' + data['employer'].lower())
+        jobs[data['id']] = data
+
+loader = jinja2.FileSystemLoader('templates')
+env = jinja2.Environment(loader=loader)
+template = env.get_template('index.html')
+template.stream(jobs=jobs).dump('output/index.html')
+
+for job in jobs.values():
+    template = env.get_template('job.html')
+    template.stream(job=job).dump(os.path.join('output/jobs', job['slug'] + '.html'))
